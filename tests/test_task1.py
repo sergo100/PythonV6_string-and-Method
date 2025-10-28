744aea9fd2621e1e56473d05cdd3ec7a7c5a3d40
Сергей Сергиенко <sergo100@yandex.ru>
2025-10-28 22:04:39 +0500

    Упорядочен импорт и добавлено обновление sys.path для корректного импорта модулей

--- tests/test_task1.py
+++ tests/test_task1.py
@@ -1,6 +1,12 @@
 import io
 import sys
+import os
+
+# Добавляем корневую директорию проекта в sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from tasks.task1 import solve
+
 
 def run_io_fun(input_data):
     sys.stdin = io.StringIO(input_data)
@@ -8,8 +14,10 @@ def run_io_fun(input_data):
     solve()
     return sys.stdout.getvalue().strip()
 
+
 def test_sample_1():
     assert run_io_fun("Монти Пайтон\n") == "МниПйо"
+
 
 def test_sample_2():
     assert run_io_fun("Цой жив!\n") == "Цйжв"
